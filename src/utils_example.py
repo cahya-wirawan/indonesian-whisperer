e1f23623cde48776ad59cb9f7322e188a60dc29b
Cahya Wirawan <cahya.wirawan@gmail.com>
2022-12-16 16:57:00 +0100

    add an example how to use load_dataset_combination

--- src/utils_example.py
+++ src/utils_example.py
@@ -2,5 +2,5 @@ from utils import load_dataset_combination
 
 ds = load_dataset_combination("mozilla-foundation/common_voice_11_0, mozilla-foundation/common_voice_10_0",
                               "lg, rw", split="train+validation, train", dataset_data_dir=",",
-                              streaming=True,  use_auth_token=True)
+                              shuffle=True, streaming=True,  use_auth_token=True)
 print(next(iter(ds)))
